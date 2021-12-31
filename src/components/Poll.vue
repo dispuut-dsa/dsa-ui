56c702700f8c5ab905eb950ea111ad438d870606
gerryflap <gerry2211@gmail.com>
2021-12-31 15:21:18 +0100

    Created component and view for polls

---
+++ src/components/Poll.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="poll">
+        <b-card class="mb-4">
+
+            <div class="poll__header" aria-controls="poll-info" @click="expanded = !expanded">
+                <h3>{{poll.name}}</h3>
+            </div>
+            <b-collapse class="activity__content mt-3" id="activity-info" v-model="expanded">
+                <h5>Omschrijving: </h5>
+                <p>{{poll.description}}</p>
+
+                <h5>Stemmen: </h5>
+
+                <ul>
+                    <li v-for='option in poll.options'>
+                        {{option.option}}: {{option.count}}
+                    </li>
+                </ul>
+            </b-collapse>
+        </b-card>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "Poll",
+        props: {
+            poll: {
+                type: Object,
+                required: true
+            }
+        },
+        data() {
+            return {
+                expanded: false
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    .poll {
+        &__header {
+            cursor: pointer;
+        }
+    }
+</style>
---
+++ src/views/Polls.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="polls">
+        <b-container>
+            <h1 class="mb-4">Polls</h1>
+            <div v-if="polls !== null">
+                <Poll v-for="poll in polls" :poll="poll" :key="poll.id" />
+            </div>
+            <div v-else>
+                Laden...
+            </div>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import Backend from '../services/backend'
+    import Poll from "../components/Poll";
+
+    export default {
+        name: "Polls",
+        components: {Poll},
+        asyncComputed: {
+            async polls() {
+                return await Backend.getPolls();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
